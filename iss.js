89e29abd7b4ac21a86fa4b83ebb6a2e4308341ac
Clayton <persinger.clayton@gmail.com>
2023-05-11 19:20:59 -0700

    Removed unncessary comments from iss.js. Created 2 new files for working with promises and installed request-promise-native

--- iss.js
+++ iss.js
@@ -1,11 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
 
@@ -64,7 +56,6 @@ const nextISSTimesForMyLocation = (callback) => {
     }
 
     fetchCoordsByIP(ip, (error, latAndLong) => {
-
       if (error) {
         console.log("It didn't work!", error);
         return;
